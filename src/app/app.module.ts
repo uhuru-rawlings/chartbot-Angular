e8a96a7f5bf3c5c1b12abc501621bf656a2dc702
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 10:34:06 +0300

    added angular stracture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsComponent } from './charts/charts.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetComponent } from './reset/reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartsComponent,
+    LoginComponent,
+    SignupComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
