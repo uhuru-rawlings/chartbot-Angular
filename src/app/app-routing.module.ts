2b4a4895a9b54eb24787e924b59df22600e0ce32
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-14 17:15:18 +0300

    added 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ResetComponent } from './reset/reset.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ChartsComponent},
+  {path:'/login',component:LoginComponent},
+  {path:'/reset',component:ResetComponent},
+  {path:'/account',component:SignupComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const links = [ChartsComponent,LoginComponent,ResetComponent,SignupComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,links } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartsComponent } from './charts/charts.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ResetComponent } from './reset/reset.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartsComponent,
-    LoginComponent,
-    SignupComponent,
-    ResetComponent
+    links,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
