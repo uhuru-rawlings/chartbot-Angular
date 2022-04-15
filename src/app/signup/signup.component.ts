cf7a0373544cc8fe2dc5a2b7c098db1e80872db5
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-15 22:00:31 +0300

    added login function

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SignupService } from '../services/signup.service';
+import { LoginService } from '../services/login.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -15,7 +16,7 @@ export class SignupComponent implements OnInit {
   // login
   phonenumbers:any = ''
   passwords:any = ''
-  constructor(private SignupService:SignupService) { }
+  constructor(private SignupService:SignupService, private LoginService:LoginService) { }
 
   ngOnInit(): void {
   }
@@ -41,5 +42,14 @@ export class SignupComponent implements OnInit {
         this.success = ''
         this.error = 'please fill all the required details.'
       }
+  }
+
+  loginuser(){
+    if(this.phonenumbers != '' || this.passwords != ''){
+      let credentials = {
+        'phonenumber':this.phonenumbers,
+        'password':this.passwords
+      }
+    }
   }
 }
